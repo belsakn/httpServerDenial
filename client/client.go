d497639920be8843f7b3c3901e3044a1f84aee2d
mali8 <belsak@gmail.com>
2018-05-12 21:43:17 +0200

    handle CTRL+C event & minor logic refactor

--- client/client.go
+++ client/client.go
@@ -1,26 +1,42 @@
 package client
 
 import (
+	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func Client() {
 
-	for {
-		makeGetRequest("http://localhost:8080/?clientId=2")
-		r := rand.Intn(1222)
-		time.Sleep(time.Duration(r) * time.Millisecond)
-	}
+	setupCloseHandler()
+
+	startClient(fmt.Sprintf("http://localhost:8080/?clientId=1"))
 
 }
 
-func makeGetRequest(url string) {
-	resp, err := http.Get(url)
-	if err != nil {
-		panic(err)
+func startClient(url string) {
+	for {
+		resp, err := http.Get(url)
+		if err != nil {
+			panic(err)
+		}
+		log.Printf("Get %s response code is %d", url, resp.StatusCode)
+		r := rand.Intn(1222)
+		time.Sleep(time.Duration(r) * time.Millisecond)
 	}
-	log.Printf("Get %s response code is %d", url, resp.StatusCode)
+}
+
+func setupCloseHandler() {
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Println("# Ctrl+C pressed")
+		os.Exit(0)
+	}()
 }
