94ee16e224c2c7c1c7aeca6893379ed8d57d2250
mali8 <belsak@gmail.com>
2018-05-13 17:51:07 +0200

    Run desired number of clients at the same time. Wait for client to finish when pressed CTRL+C

--- client/client.go
+++ client/client.go
@@ -7,28 +7,45 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"time"
 )
 
-func Client() {
+var wg sync.WaitGroup
+var stopWorking bool = false
+
+func Client(numberOfClinets int) {
 
 	setupCloseHandler()
 
-	startClient(fmt.Sprintf("http://localhost:8080/?clientId=1"))
+	for i := 0; i <= numberOfClinets; i++ {
+		wg.Add(1)
+		go startClient(fmt.Sprintf("http://localhost:8080/?clientId=%d", i), i)
+	}
 
+	wg.Wait()
+	log.Printf("My work have is done...")
 }
 
-func startClient(url string) {
+func startClient(url string, id int) {
 	for {
 		resp, err := http.Get(url)
 		if err != nil {
 			panic(err)
 		}
+
 		log.Printf("Get %s response code is %d", url, resp.StatusCode)
+
+		if stopWorking {
+			break
+		}
+
 		r := rand.Intn(1222)
 		time.Sleep(time.Duration(r) * time.Millisecond)
 	}
+	fmt.Println("# Client with id: ", id, " stoped making requests.")
+	wg.Done()
 }
 
 func setupCloseHandler() {
@@ -36,7 +53,7 @@ func setupCloseHandler() {
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
-		fmt.Println("# Ctrl+C pressed")
-		os.Exit(0)
+		fmt.Println("# Wait for all Clients to finish their requests...")
+		stopWorking = true
 	}()
 }
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"interview-mali8/client"
 	"interview-mali8/server"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -14,7 +15,20 @@ func main() {
 		case "server":
 			server.Server()
 		case "client":
-			client.Client()
+			if len(os.Args) < 3 {
+				fmt.Printf("\n Provide client argument! \n")
+				printUsage()
+				break
+			}
+
+			numberOfClients, err := strconv.Atoi(os.Args[2])
+			if err != nil {
+				fmt.Printf("\n Provide client argument as whole number! \n")
+				printUsage()
+				break
+			}
+
+			client.Client(numberOfClients)
 		case "help":
 			printUsage()
 		default:
@@ -23,14 +37,13 @@ func main() {
 	} else {
 		printUsage()
 	}
-
 }
 
 func printUsage() {
 	fmt.Printf("\n Usage: \n\n \t interview-mali8 command [arguments]")
 	fmt.Printf("\n\n The commands are: \n")
 	fmt.Printf("\n \t server \t\t run HTTP Denial-of-Service protection system on port 8080")
-	fmt.Printf("\n \t client [clientNum] \t run clientNum HTTP clients")
+	fmt.Printf("\n \t client [clientNum] \t run clientNum of HTTP clients")
 	fmt.Printf("\n \t help \t\t\t print usage instructions")
 	fmt.Printf("\n")
 }
